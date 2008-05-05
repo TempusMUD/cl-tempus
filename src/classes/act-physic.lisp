e1b938692dfc3d729b92932aa0de5476d552f41a
Daniel Lowe <dlowe@dlowe.net>
2008-05-05 06:42:10 -0400

    Errors loading timewarp file are now handled.

--- src/classes/act-physic.lisp
+++ src/classes/act-physic.lisp
@@ -3,9 +3,12 @@
 (defvar *timewarps* nil)
 
 (defun boot-timewarp-data ()
-  (with-open-file (inf +timewarp-file+ :direction :input)
-    (setf *timewarps*
-          (loop for warp-rooms = (read inf nil)
-               while warp-rooms
-               collect warp-rooms))
-    (slog "Timewarp data booted, ~d elements read." (length *timewarps*))))
+  (handler-case
+      (with-open-file (inf +timewarp-file+ :direction :input)
+        (setf *timewarps*
+              (loop for warp-rooms = (read inf nil)
+                 while warp-rooms
+                 collect warp-rooms))
+        (slog "Timewarp data booted, ~d elements read." (length *timewarps*)))
+    (error (err)
+        (slog "Timewarp error: ~a" err))))
