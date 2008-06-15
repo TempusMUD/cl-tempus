0484a88f9957bb851d0518ebc6d6a78c016f5002
Daniel Lowe <dlowe@dlowe.net>
2008-06-15 09:56:29 -0400

    randomization functions now work better with ranges with no variance

--- src/util/random.lisp
+++ src/util/random.lisp
@@ -1,20 +1,22 @@
 (in-package #:tempus)
 
 (defun random-range (min max)
-  "creates a random number in interval [from,to]"
-  (+ min (random (- max min))))
+  "Creates a random number in interval [from,to]"
+  (if (= min max)
+      min
+      (+ min (random (- max min)))))
 
 (defun dice (num size)
-  "simulates dice roll"
+  "Simulates dice roll"
   (if (or (minusp num) (minusp size))
       0
       (loop for idx from 1 upto num
            sum (random-range 1 size))))
 
 (defun rand-value (val variance min max)
-  (let ((variance-min (- val variance))
-        (variance-max (+ val variance)))
-    (if (zerop variance)
-        val
+  (if (zerop variance)
+      val
+      (let ((variance-min (- val variance))
+            (variance-max (+ val variance)))
         (random-range (if min (max min variance-min) variance-min)
                       (if max (min max variance-max) variance-max)))))
