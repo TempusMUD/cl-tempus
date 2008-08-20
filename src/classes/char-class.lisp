724b0a5f1ba24bdea8a77243225f3a600492d842
Daniel Lowe <dlowe@dlowe.net>
2008-08-20 16:20:51 -0400

    Fixed typo in invalid-char-class

--- src/classes/char-class.lisp
+++ src/classes/char-class.lisp
@@ -27,7 +27,7 @@
          (and (is-obj-stat obj +item-anti-warrior+) (is-warrior ch))
          (and (is-obj-stat obj +item-anti-thief+) (is-thief ch))
          (and (is-obj-stat obj +item-anti-barb+) (is-barb ch))
-         (and (is-obj-stat obj +item-anti-psychic+) (is-psychic ch))
+         (and (is-obj-stat obj +item-anti-psychic+) (is-psionic ch))
          (and (is-obj-stat obj +item-anti-physic+) (is-physic ch))
          (and (is-obj-stat obj +item-anti-cyborg+) (is-cyborg ch))
          (and (is-obj-stat obj +item-anti-knight+) (is-knight ch))
@@ -59,5 +59,5 @@
      nil)
     (t
      t)))
-     
+
 
