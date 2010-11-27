a5457000cf68ec0b089485de340d5379b84d1c06
Daniel Lowe <dlowe@dlowe.net>
2010-11-26 20:27:09 -0500

    Added aliases for the hit command

--- src/combat/act-offensive.lisp
+++ src/combat/act-offensive.lisp
@@ -27,15 +27,47 @@
 (defun perform-flee (ch)
   nil)
 
-(defcommand (ch "slay") ()
+(defcommand (ch "attack") (:standing)
+  (send-to-char ch "Attack who?~%"))
+
+(defcommand (ch "attack" target) (:standing)
+  (perform-hit ch (first (resolve-alias ch target (people-of (in-room-of ch))))))
+
+(defcommand (ch "fight") (:standing)
+  (send-to-char ch "Fight who?~%"))
+
+(defcommand (ch "fight" target) (:standing)
+  (perform-hit ch (first (resolve-alias ch target (people-of (in-room-of ch))))))
+
+(defcommand (ch "hit") (:standing)
+  (send-to-char ch "Hit who?~%"))
+
+(defcommand (ch "hit" target) (:standing)
+  (perform-hit ch (first (resolve-alias ch target (people-of (in-room-of ch))))))
+
+(defcommand (ch "kill") (:standing)
   (send-to-char ch "Kill who?~%"))
 
-(defcommand (ch "slay" target) (:immortal)
+(defcommand (ch "kill" target) (:standing)
+  (perform-hit ch (first (resolve-alias ch target (people-of (in-room-of ch))))))
+
+(defcommand (ch "murder") (:standing)
+  (send-to-char ch "Murder who?~%"))
+
+(defcommand (ch "murder" target) (:standing)
+  (perform-hit ch (first (resolve-alias ch target (people-of (in-room-of ch))))))
+
+(defcommand (ch "slay") (:standing)
+  (send-to-char ch "Slay who?~%"))
+
+(defcommand (ch "slay" target) (:standing)
   (let* ((targets (resolve-alias ch target (people-of (in-room-of ch))))
          (tch (first targets)))
     (cond
       ((null targets)
        (send-to-char ch "They aren't here.~%"))
+      ((not (immortalp ch))
+       (perform-hit ch (first (resolve-alias ch target (people-of (in-room-of ch))))))
       ((find ch targets)
        (send-to-char ch "Your mother would be so sad... :(~%"))
       (t
@@ -48,16 +80,3 @@
                (name-of tch)
                (name-of (in-room-of ch)))
        (raw-kill tch ch +type-slash+)))))
-
-(defcommand (ch "hit" target) ()
-  (perform-hit ch (first (resolve-alias ch target (people-of (in-room-of ch))))))
-
-(defcommand (ch "kill") ()
-  (send-to-char ch "Kill who?~%"))
-
-(defcommand (ch "kill" target) ()
-  (let ((victims (resolve-alias ch target (people-of (in-room-of ch)))))
-    (cond
-      ((null victims)
-       (send-to-char ch "They aren't here.~%"))
-      (()))))
